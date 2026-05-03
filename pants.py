31d28cec6f77e62e1b32cfe6155ca3582d91265b
artyom-e <emelkin.artem@icloud.com>
2026-05-03 18:10:26 +0300

    version 2

---
+++ pants.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+headers = {'User-Agent': 'Mozilla/5.0'}
+base_url = 'https://ooomalina.ru/catalog/zhenskij-trikotazh/bryuki-bridzhi-shorty-zhenskie-trikotazhnye/'
+
+def get_data_parents():
+    data = []
+    for page in range(1, 5):
+        URL = f'{base_url}?PAGEN_1={page}/'
+
+        response = requests.get(URL, headers=headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        products = soup.find_all('div', class_='item_info')
+
+        for prod in products:
+            title = prod.find('div', class_='item-title')
+            price = prod.find('span', class_='price_value')
+            price_currency = prod.find('span', class_='price_currency')
+            ref = prod.find('a')['href']
+
+            if title and price and price_currency:
+                data.append(
+                    {'название': title.text.strip(), 'цена': price.text.strip() + ' ' + price_currency.text.strip(),
+                     'ссылка': ref})
+
+    df = pd.DataFrame(data)
+    df.to_excel('parents_data.xlsx', index=False)
+
+
+
