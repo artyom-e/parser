5a97c4291338e9f74115c7ad868f07c0c89a64e5
artyom-e <emelkin.artem@icloud.com>
2026-05-03 12:10:07 +0300

    upgrade

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ import pandas as pd
 headers = {'User-Agent': 'Mozilla/5.0'}
 URL = 'https://ooomalina.ru/catalog/zhenskij-trikotazh/platya-sarafany-zhenskie-trikotazhnye/'
 response = requests.get(URL, headers=headers)
-soup = BeautifulSoup(response.content, 'html.parser')
+soup = BeautifulSoup(response.text, 'html.parser')
 
-products = soup.find_all('div', class_='price font-bold font_mxs')
+products = soup.find_all('div', class_='item_info')
 
-products_title = soup.find_all('div', class_='item-title')
-products_list = []
-costs_list = []
-for title in products_title:
-    products_list.append(title.text.strip())
+data = []
 
-i = 0
-for prodict in products:
-    if i % 3 == 0: #due to cost is printed 3 times, we print the every third element
-        costs_list.append(prodict.text.strip()[:-3])
-    i += 1
+for prod in products:
+    title = prod.find('div', class_='item-title')
+    price = prod.find('span', class_='price_value')
+    price_currency = prod.find('span', class_='price_currency')
 
-df = pd.DataFrame({'название': products_list, 'цена': costs_list})
-print(df)
+    if title and price and price_currency:
+        data.append(
+            {'название': title.text.strip(), 'цена': price.text.strip() + ' ' + price_currency.text.strip(),})
+
+df = pd.DataFrame(data)
+
+df.to_excel('data.xlsx', index=False)
